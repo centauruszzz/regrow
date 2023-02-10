d0c146a6b063911dafa6dcc14cf5b47979c1deda
centauruszzz <52918780+centauruszzz@users.noreply.github.com>
2023-02-10 02:16:30 +0000

    init

--- frontend/miniprogram/pages/play/play.js
+++ frontend/miniprogram/pages/play/play.js
@@ -17,32 +17,39 @@ Page({
       },
       alpha:0
     }],
-    scale:3
+    scale:3,
+    toView:null,
+    reverse:false
   },
 
   onLoad:function(){
     this.schoolIndex=0;
     this.mapCtx = wx.createMapContext('map', this);
+    this.height=0;
   },
 
   onClick:function(){
     if(this.data.button_msg=="投胎" || this.data.button_msg=="重新投胎"){
       this.getLife(()=>{
-        let birthPlace=this.json["birthPlace"];
-        this.setData({msgList:[],button_msg:"成长"});
+        this.setData({msgList:[],button_msg:"成长",scrolltop:0});
         this.schoolIndex=0;
+        let birthPlace=this.json["birthPlace"];
         this.addMsg(`出生于${birthPlace["area"]}`);
-        this.onChangeView(birthPlace.lng,birthPlace.lat,birthPlace["area"],7);
+        this.onChangeView(birthPlace.lng,birthPlace.lat,birthPlace["area"],8);
       })
     }else if(this.data.msgList.length<=this.json["schoolList"].length){
       let school=this.json["schoolList"][this.schoolIndex];
       this.addMsg(`${school["level"]}就读于${school["name"]}`);
       this.schoolIndex++;
-      this.onChangeView(school.lng,school.lat,school.name,15);
+      this.onChangeView(school.lng,school.lat,school.name,16);
     }else{
-      let industry=this.json["industry"]
-      this.addMsg(`定居于${industry["province"]}，从事${industry["industry"]}，工资${industry["salary"]}`)
-      this.setData({button_msg:"重新投胎"});
+      let industry=this.json["industry"];
+      this.addMsg(`定居于${industry["province"]}，从事${industry["industry"]}，工资${industry["salary"]}`);
+      let marker=this.data.markers[0];
+      marker.alpha=0;
+      marker.callout.content=null;
+      this.setData({button_msg:"重新投胎",scale:3,markers:[marker]});
+      
     }
 
   },
@@ -51,6 +58,7 @@ Page({
     let msgList=this.data.msgList;
     msgList.push({message:msg});
     this.setData({msgList,msgList});
+    this.setData({toView:`item${this.data.msgList.length-1}`})
   },
 
   getLife:async function(callback){
@@ -59,9 +67,23 @@ Page({
       header:{
         "X-WX-SERVICE": "cloud"
       },
+      data:{
+        reverse:this.data.reverse
+      }
     });
     this.json=res.data;
     callback();
+    // wx.request({
+    //   url: 'http://127.0.0.1:5000/birth',
+    //   data:{
+    //     reverse:this.data.reverse
+    //   },
+    //   success:(res)=>{
+    //     this.json=res.data;
+    //     callback();
+    //   }
+    // })
+    
   },
 
   onChangeView:function(lng,lat,name,scale){
@@ -74,13 +96,15 @@ Page({
     this.mapCtx.getCenterLocation({
       success:(res)=>{
         if(Math.abs(lng-res.longitude)>2 || Math.abs(lat-res.latitude)>2){
-          this.setData({scale:3});
-          setTimeout(() => {this.mapCtx.moveToLocation({latitude:lat,longitude:lng});this.setData({markers:[marker]});},1000);    
-          setTimeout(() => this.setData({markers:[marker],scale:scale}), 2000);
+          this.setData({scale:3})
+          this.mapCtx.moveToLocation({latitude:lat,longitude:lng,success:()=>this.setData({markers:[marker],scale:scale})});   
         }else{
-          this.setData({markers:[marker],scale:scale})
-          this.mapCtx.moveToLocation({latitude:lat,longitude:lng})          
+          this.mapCtx.moveToLocation({latitude:lat,longitude:lng,success:()=>this.setData({markers:[marker],scale:scale})});
         }
     }})
+  },
+
+  onReverse:function(){
+    this.setData({reverse:!this.data.reverse});
   }
 })
